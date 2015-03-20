12ed4c4c1fc54cf048c9e68f641da5dce14b62f4
Antoine Aflalo <antoine@aaflalo.me>
2015-03-20 17:52:32 +0200

    Encode link in base64 Updated hitcounter for the episode

--- api/ddl.php
+++ api/ddl.php
@@ -7,26 +7,35 @@ header("Content-Type: application/json");
  * Time: 17:09
  */
 require_once('../admin/conf.php');
-$id = $_POST['id'];
-$type = $_POST['type'];
 
-if(!is_numeric($id)) {
+if (!isset($_POST['id']) || !isset($_POST['type'])) {
     header("HTTP/1.1 404 Not Found");
     echo '{}';
     return;
 }
 
-$sql= 'SELECT filepath FROM DirectDownloads WHERE episode=? AND type = ?';
-$db->pQuery($sql,array('is',$id,$type));
+$id = $_POST['id'];
+$type = strtoupper($_POST['type']);
+
+if (!is_numeric($id)) {
+    header("HTTP/1.1 404 Not Found");
+    echo '{}';
+    return;
+}
+
+$sql = 'SELECT filepath FROM DirectDownloads WHERE episode=? AND type = ?';
+$db->pQuery($sql, array('is', $id, $type));
 $link = $db->getVar();
 
-if(empty($link)) {
+if (empty($link)) {
     header("HTTP/1.1 404 Not Found");
     echo '{}';
     return;
 }
 
 $object = new stdClass();
-$object->dlpath = $link;
+$object->dlpath = base64_encode($link);
 
 echo json_encode($object);
+$requete = 'UPDATE downloads SET nbhits=nbhits+1 WHERE id=?';
+$db->pQuery($requete, array('i', $id));
